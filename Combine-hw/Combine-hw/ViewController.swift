a297aebc55807d47156cfb23096817c9c81ed5a2
Rustem <47086556+ironrustem@users.noreply.github.com>
2021-11-09 14:04:08 +0300

    [INIT] Init project.

---
+++ Combine-hw/Combine-hw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Combine-hw
+//
+//  Created by wrustem on 09.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
